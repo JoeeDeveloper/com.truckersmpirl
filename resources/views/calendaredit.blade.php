9089e56428180dd85c196f434cf2a9896dcee498
Joe Alderdice <jgalderdice@outlook.com>
2020-04-09 19:52:52 +0100

    datepicker actually works

--- resources/views/calendaredit.blade.php
+++ resources/views/calendaredit.blade.php
@@ -2,18 +2,15 @@
 @section('head')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css" />
 @endsection
-{{-- @section('js')
+ @section('js')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script>
-    $(document).ready(function () {
-
-        $('#input-group .input-group.date').datepicker({
-            todayBtn: "linked"
-        });
-
-    })
+    $('.input-daterange').datepicker({
+        format: "dd/mm/yyyy",
+        weekStart: 1
+    });
 </script>
-@endsection --}}
+@endsection --
 @section('content')
 <div class="col-md-6">
     <div class="card">
@@ -30,15 +27,16 @@
                         placeholder="Enter event description.">
                 </div>
                 <div class="form-group">
-                    {{-- <div class="input-daterange input-group" id="datepicker">
-                        <input type="text" class="input-sm form-control" name="start"/>
+                    <label for="date">Event Date</label>
+                    <div class="input-daterange input-group" id="datepicker">
+                        <input type="text" class="input-sm form-control" name="start" />
                         <span class="input-group-addon">to</span>
                         <input type="text" class="input-sm form-control" name="end" />
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button> --}}
-                    <div class="input-group">
-                    <input type="text" class="form-control"><span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
-                    </div>
+                    <br>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
                 </form>
             </div>
         </div>
